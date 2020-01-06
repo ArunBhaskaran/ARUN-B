59a5867a5562e5db980320cd753279e7f7350838
ARUN B <58020161+ArunBhaskaran@users.noreply.github.com>
2020-01-06 15:43:35 +0530

    Update book1.java

--- book1.java
+++ book1.java
@@ -5,14 +5,13 @@ import java.io.IOException;
 import java.util.Scanner;
 public class eid1
 {
-	
  public String change() throws IOException
  {
 	 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	 String nemail;
-System.out.println("Update email");
-System.out.println("Enter the new Email ID");
- nemail=in.readLine();
- return nemail;
+         System.out.println("Update email");
+        System.out.println("Enter the new Email ID");
+	 nemail=in.readLine();
+ 	return nemail;
  }
 }
